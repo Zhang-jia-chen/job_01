4f23bb31e34093b009442b7c7097c9e1743d2114
Zhang-jia-chen <zjc834723074@gmail.com>
2018-03-17 11:01:33 +0800

    implement wage part

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,7 +1,7 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   before_action :require_is_admin
-  
+
   def show
     @job = Job.find(params[:id])
 
@@ -39,6 +39,6 @@ class Admin::JobsController < ApplicationController
     redirect_to admin_jobs_path
   end
   private
-  params.require(:job).permit(:title, :description)
+  params.require(:job).permit(:title, :description,:wage_upper_bound,:wage_lower_bound,:contact_email)
 
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -11,7 +11,7 @@ class JobsController < ApplicationController
 
   end
   def create
-    @job = Job.new(params.require(:job).permit(:title, :description))
+    @job = Job.new(params_job)
 
     if @job.save
       redirect_to jobs_path
@@ -41,6 +41,6 @@ class JobsController < ApplicationController
 
   private
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description,:wage_upper_bound,:wage_lower_bound,:contact_email)
   end
 end
