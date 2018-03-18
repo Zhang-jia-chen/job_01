2ecd87fb82cee1fde0dbeb81680fc81934ebf805
Zhang-jia-chen <zjc834723074@gmail.com>
2018-03-18 15:20:14 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,9 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
-      resources :resumes
+    resources :resumes
     end
+
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
