1da4074760bf4534a81734d6de039164b5e58941
Zhang-jia-chen <zjc834723074@gmail.com>
2018-03-18 12:25:15 +0800

    step 7 upload resume

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180317075232) do
+ActiveRecord::Schema.define(version: 20180318035739) do
 
   create_table "jobs", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20180317075232) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "attachment"
   end
 
   create_table "users", force: :cascade do |t|
